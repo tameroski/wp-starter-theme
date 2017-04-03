7741e27237205723b521ed9aeb18d86eff71e395
Tameroski <jgrosdenier@gmail.com>
2017-04-03 16:24:39 +0200

    Oups

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,18 +2,7 @@
 	"use strict";
 
 	$(function () {
-		$("#scene").animsition({
-		    inClass: "fade-in-down",
-		    outClass: "fade-out-up",
-		    inDuration: 1500,
-		    outDuration: 800,
-		    linkElement: "a:not([target=\"_blank\"]):not([href^=\"#\"])",
-		    loading: false,
-		    timeout: false,
-		    overlay : true,
-		    overlayClass : "animsition-overlay-slide",
-		    overlayParentElement : "body",
-		});
+		// DOM ready, take it away
 	});
 
 } ( this, jQuery ));
